d38935de8642097a5fdde931fe1943c3a3d29228
Arvind Suthar <35096203+Arvind-Suthar@users.noreply.github.com>
2021-12-24 16:28:26 +0530

    Add files via upload

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,15 +1,6 @@
 let currentActive = 0;
-
 const sections = document.querySelectorAll("section");
 const links = document.querySelectorAll(".item");
-/*window.addEventListener("scroll", () => {
-
-    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin ) - 1
-
-    if (current !== currentActive) {
-        
-    }
-});*/
 const windowInnerWidth  = document.documentElement.clientWidth;
 const breakId = 19 - Math.floor(windowInnerWidth/115);
 window.onscroll = () => {
@@ -21,14 +12,12 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
         let idInt = parseInt(id.substr(4,id.length-1));   
         if(top >= offset && top < (offset+height)){
-            if(idInt <= breakId){
-                links.forEach((link) => {
-                    link.classList.remove('active');
-                    document.querySelector(`.item[data-circle-year=${id}]`).classList.add('active');
-                                    
-                    
-                });
+            links.forEach((link) => {
+                link.classList.remove('active');
+                document.querySelector(`.item[data-circle-year=${id}]`).classList.add('active');
 
+            });
+            if(idInt <= breakId){
                 let scrollingOffset = (idInt)*115;
                 if(scrollingOffset>115){
                     document.querySelector("#scroller").style.marginLeft = -(scrollingOffset-115)+"px";
