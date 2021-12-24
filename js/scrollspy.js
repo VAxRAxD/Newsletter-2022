24ebb180dd53f87ad3a3056eaae45a2babfc72cd
Arvind Suthar <35096203+Arvind-Suthar@users.noreply.github.com>
2021-12-24 16:22:48 +0530

    Add files via upload

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -10,27 +10,32 @@ const links = document.querySelectorAll(".item");
         
     }
 });*/
-
+const windowInnerWidth  = document.documentElement.clientWidth;
+const breakId = 19 - Math.floor(windowInnerWidth/115);
 window.onscroll = () => {
     sections.forEach(sec =>{
         let top = window.scrollY;   
         let offset = sec.offsetTop; 
         offset-=96; 
         let height = sec.offsetHeight;  
-        let id = sec.getAttribute('id');   
+        let id = sec.getAttribute('id');
+        let idInt = parseInt(id.substr(4,id.length-1));   
         if(top >= offset && top < (offset+height)){
-            links.forEach((link) => {
-                link.classList.remove('active');
-                document.querySelector(`.item[data-circle-year=${id}]`).classList.add('active');
-                                   
-                
-            });
-            let scrollingOffset = (parseInt(id.substr(4,id.length-1)))*115;
-            if(scrollingOffset>115){
-                document.querySelector("#scroller").style.marginLeft = -(scrollingOffset-115)+"px";
-            }else{
-                document.querySelector("#scroller").style.marginLeft = 0+"px";
-            } 
+            if(idInt <= breakId){
+                links.forEach((link) => {
+                    link.classList.remove('active');
+                    document.querySelector(`.item[data-circle-year=${id}]`).classList.add('active');
+                                    
+                    
+                });
+
+                let scrollingOffset = (idInt)*115;
+                if(scrollingOffset>115){
+                    document.querySelector("#scroller").style.marginLeft = -(scrollingOffset-115)+"px";
+                }else{
+                    document.querySelector("#scroller").style.marginLeft = 0+"px";
+                } 
+            }
         }
     });
 };
