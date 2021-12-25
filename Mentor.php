d7074d65e45dd0cf68f23b2c39726cec378ddcf5
Arvind Suthar <35096203+Arvind-Suthar@users.noreply.github.com>
2021-12-25 16:59:40 +0530

    Add files via upload

---
+++ Mentor.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+    <link rel="stylesheet" href="./css/master_Theme1.css" />
+    <title>From the Desks - Mentor</title>
+  </head>
+  <body>
+  <?php include("Header.php"); ?>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-2 col-lg-1 side-col lg-h1 h-100">
+          <h1>
+            <span>M</span>
+            <span>E</span>
+            <span>N</span>
+            <span>T</span>
+            <span>O</span>
+            <span>R</span>
+          </h1>
+        </div>
+        <div class="col-sm col-lg-11 h-100 wrap" style="overflow-y: scroll">
+          <div class="ribbon fromdesks-ribbon">
+            <img alt="ribbon" class="img-fluid" />
+          </div>
+          <div class="container-fluid a-center">
+            <div class="row">
+              <div class="col-md-4 flex-center">
+                <div class="img-wrap">
+                  <img
+                    src="./images/credits/mentor.png"
+                    alt="Ms. Grinal Tuscano"
+                    class="img-fluid mentor-img"
+                  />
+                  <span>Ms. Grinal Tuscano</span>
+                </div>
+              </div>
+              <div class="col-md">
+                <div class="side-col sm-h1">
+                  <h1>
+                    <span>M</span>
+                    <span>E</span>
+                    <span>N</span>
+                    <span>T</span>
+                    <span>O</span>
+                    <span>R</span>
+                  </h1>
+                </div>
+                <p class="mentor-text">
+                  Every moment we've lived and every decision we've held on to
+                  has contributed to our today. While the laws of the world
+                  forbid us to go back and relive a day of the past, aren't we
+                  blessed to be able to hold on to those memories? We have some
+                  vivid memories, some hazy and some forgotten, but they make us
+                  individuals. Isn't it beautiful how 7.9 billion humans are
+                  unique because of their experiences? That's the power of the
+                  past. It makes our story. Sometimes, I sit by the window and
+                  smile as I recollect the memories of my college days. I
+                  believe I'm not the only one who smiles thinking about
+                  college. Our college days are probably the best days of our
+                  youth. We started making our own decisions, understanding new
+                  relationships, trusting friendships, partying, studying,
+                  bunking and so much more during those days. Many volunteered,
+                  some took up leadership opportunities, several studied and a
+                  few just did not. Oh, how I wish we could relive those days,
+                  just one more time. SFIT and SFITAA have left no opportunities
+                  to build such memories. Every society and event has been
+                  designed to give our students memories to love, laugh and live
+                  forever. One such event this year would be the 'Grand
+                  Unearthing of the 2012 Time Capsule'. A treasure chest of
+                  photos, stories, artifacts shared by our students and teachers
+                  of 2012 (who contributed to the time capsule and what does it
+                  hold?). Therefore we reached out to our alumni to find out
+                  what they would like to preserve if they were to contribute to
+                  the new time capsule. We've been sprinkling those alumni
+                  testimonials ever since our last newsletter and we even have a
+                  new section named ‘Mini Stories’. If you haven't read them
+                  yet, do check them out! There's more in this issue and a lot
+                  more coming up. Excited as much as we are? We'd love to hear
+                  back from you.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
---
+++ foreword.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="stylesheet" href="./css/master_Theme2.css" />
+    <title>Foreword</title>
+  </head>
+
+  <body>
+  <?php include("Header.php"); ?>
+    <div class="container">
+      <div class="ribbon foreword-ribbon">
+        <img alt="ribbon" class="img-fluid" />
+      </div>
+    </div>
+    <div class="container-fluid foreword-container">
+      <div class="row justify-content-center foreword-header">
+        <div class="col-md-4 d-flex justify-content-center">
+          <div class="foreword-img-wrap d-flex justify-content-center align-items-center">
+            <img src="./images/Foreword/Hiren Tank.jpeg" alt="Foreword-Image" class="img-fluid" />
+          </div>
+        </div>
+        <div class="col-md-8 foreword-name">
+          <h3>Mr. Hiren Tank</h3>
+          <h4>Joint Treasurer</h4>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row justify-content-end">
+        <div class="col-md-8">
+          <p class="foreword-para">
+            What started as a reboot in a new avatar, back in July 2020,
+            ‘Chrysalis Konnect’, our biannual Alumni Newsletter, just in its 4th
+            edition, has already received fantastic response and positive
+            feedback which is exciting and simply inspiring!
+          </p>
+          <p class="foreword-para">
+            To me, this edition – and the previous one as well – illustrates how
+            we all were able to persist and innovate through these trying times.
+            Despite the challenges; the college, students and the SFITAA team
+            have been putting in all their efforts to ensure that the business
+            runs as usual in the Alumni office. A befitting example has to be
+            the Rekindle ‘21 event – a first of its kind virtual reunion we’ve
+            ever seen, more than 15 batches participated remotely without any
+            hassles, and what a reception! An event entirely conceived,
+            designed, managed and run by our very own people and within our very
+            own playground. The participating numbers in the event proves that
+            nothing can stop us from reconnecting and reliving our old memories
+            together. Adding to this are the many webinars and virtual
+            conferences that were successfully organized and executed during
+            this period. I would encourage all to visit and relive these through
+            our Alumni archives. A BIG shout out to one and all who were
+            directly or indirectly involved in making these events a success!
+          </p>
+
+          <p class="foreword-para">
+            Coming back to our newsletter, along with its other interesting news
+            and articles, this edition will also speak about unveiling of a
+            historic and important event in 2022, ‘Unearthing of the SFIT Time
+            Capsule’ which was planted by our fellow friends and faculty members
+            in IRIS ‘12 (themed Foresight). A much-awaited event which will make
+            us reflect on our choices and changes that have taken place within
+            us and our surroundings in the past decade. Appreciate all the hard
+            work put in by the team in creating, compiling and editing this
+            newsletter; some interesting content follows here, so happy reading
+            folks!
+          </p>
+          <p class="foreword-para">
+            As much as you’ll have been hearing from SFITAA, SFITAA wants to
+            hear from you too. We urge you to think of ways in which you can
+            contribute back to the institute to whatever degree you are
+            comfortable – be it, career and academic counselling or mentoring,
+            volunteering time at on and off campus events, or by simply
+            attending academic and social events. SFITAA encourages you to
+            follow and explore all that is on offer to stay connected through
+            our website and our social networking sites. We’d love to hear about
+            your stories and accomplishments!
+          </p>
+          <p class="foreword-para">
+            Wish you and your family better health and prosperity in the New
+            Year!
+          </p>
+          <p class="foreword-para">Best wishes and warm regards.</p>
+          <!-- <p class="foreword-para">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
+            dicta, quaerat dolore molestias totam, illo voluptatem dolorem atque
+            autem at iure exercitationem, nam esse tenetur distinctio unde nisi
+            ut iusto.
+          </p>
+          <p class="foreword-para">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
+            dicta, quaerat dolore molestias totam, illo voluptatem dolorem atque
+            autem at iure exercitationem, nam esse tenetur distinctio unde nisi
+            ut iusto. Praesentium, ex architecto! At nulla facere pariatur
+            magnam nihil ea doloribus saepe maxime, animi dolorum iure neque
+            quia accusantium inventore impedit dicta ipsa culpa asperiores
+            commodi veritatis adipisci? Sed, neque! Totam molestiae optio
+            voluptas vero, architecto, cupiditate vel rerum tempore recusandae
+            repellendus
+          </p> -->
+        </div>
+      </div>
+    </div>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
